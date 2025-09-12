5fa309fd2d1b47b022df29956449c97f2d660a54
Soumajit Majumder <majumdersoumait09@gmail.com>
2025-09-12 19:50:29 +0530

    write logic so that home component will not appear on seller dashbaord and banner on home for all type of screens

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,10 +9,14 @@ const App = () => {
 
   return (
     <div>
-      <Navbar/>
-      <div className='px-6 md:px-16 lg:px-24 xl:px-32'>
+      
+      {/* if it is seller dashboard then navbar will not be visible */}
+      {isSellerPath? null : <Navbar /> } 
+
+      {/* if it is seller dashboard then the banner image will not be visible */}
+      <div className={`${isSellerPath}? '' : 'px-6 md:px-16 lg:px-24 xl:px-32'`}>
         <Routes>
-          <Route path='/' element={<Home/>} />
+          <Route path='/' element={<Home />} />
         </Routes>
       </div>
     </div>
