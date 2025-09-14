919cf04785f1a4014b43f8aeb0483e131635390b
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-14 10:46:28 +0530

    add Category-

--- frontend/src/pages/ProductCategory.jsx
+++ frontend/src/pages/ProductCategory.jsx
@@ -9,18 +9,24 @@ const ProductCategory = () => {
     const { products } = useAppContext()
     const { category } = useParams()
 
+    // finding category based on the selected category 
     const searchCategory = categories.find((item) => item.path.toLowerCase() === category)
 
+    // filtering products based on the category that is selected
     const filteredProducts = products.filter((product) => product.category.toLowerCase() === category)
 
     return (
         <div className='mt-16'>
+            
+            {/* printing category name */}
             {searchCategory && (
                 <div className='flex flex-col items-end w-max'>
                     <p className='text-2xl font-medium'>{searchCategory.text.toUpperCase()}</p>
                     <div className="w-16 h-0.5 bg-primary rounded-full"></div>
                 </div>
             )}
+
+            {/* printing product based on their avaialablity */}
             {filteredProducts.length > 0 ? (
                 <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
                     {filteredProducts.map((product) => (
