0b498707c894e2155a3f62aa003daa677d0ffdd5
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-17 01:27:04 +0530

    fix some css issue 3 and add new style category selector in AddProducts page

--- frontend/src/pages/seller/AddProducts.jsx
+++ frontend/src/pages/seller/AddProducts.jsx
@@ -10,11 +10,20 @@ const AddProduct = () => {
   const [price, setPrice] = useState('')
   const [offerPrice, setOfferPrice] = useState('')
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSelect = (e) => {
+    setCategory(e)
+    setIsOpen(false);
+  };
+
+
   // function to handle submit
   const onSubmitHandler = async (event) => {
     event.preventDefault()
 
   }
+
 
 
   return (
@@ -70,7 +79,39 @@ const AddProduct = () => {
         <div className="w-full flex flex-col gap-1">
           <label className="text-base font-medium" htmlFor="category">Category</label>
 
-          <select
+          {/* category drop down menu */}
+          <div className="flex flex-col w-full text-sm relative ">
+            <button type="button" onClick={() => setIsOpen(!isOpen)}
+              className="w-full text-left px-4 pr-2 py-2 border rounded-xl bg-white text-gray-800 text-base border-gray-300  hover:bg-gray-50 focus:outline-none"
+            >
+              {
+                category ? <span>{category}</span> : <span>--Select Category--</span>
+              }
+
+              <svg className={`w-5 h-5 inline float-right transition-transform duration-200 ${isOpen ? "rotate-0" : "-rotate-90"}`} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#6B7280" >
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+              </svg>
+            </button>
+
+            {isOpen && (
+              <ul className="w-full bg-white border border-gray-300 rounded shadow-md mt-1 py-2">
+
+                <li value="" className="px-4 py-2 hover:bg-primary hover:text-white cursor-pointer"
+                  onClick={() => { handleSelect("") }}
+                >
+                  --Select Category--
+                </li>
+                {categories.map((item, index) => (
+                  <li key={index} value={item.path} className="px-4 py-2 hover:bg-primary hover:text-white cursor-pointer" onClick={() => { handleSelect(item.path) }} >
+                    {item.path}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+
+          {/* old optins */}
+          {/* <select
             onChange={(e) => setCategory(e.target.value)}
             value={category}
             id="category" className="outline-none md:py-2.5 py-2 px-3 rounded-xl border border-gray-500/40">
@@ -78,7 +119,8 @@ const AddProduct = () => {
             {categories.map((item, index) => (
               <option key={index} value={item.path}>{item.path}</option>
             ))}
-          </select>
+          </select> */}
+
         </div>
 
 
@@ -103,7 +145,7 @@ const AddProduct = () => {
           </div>
         </div>
 
-{/* add button */}
+        {/* add button */}
         <button className="px-8 py-2.5 bg-primary hover:bg-primary-dull rounded-xl text-white font-medium cursor-pointer">ADD</button>
       </form>
     </div>
