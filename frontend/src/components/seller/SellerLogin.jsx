7f9519f1055798eb1d7ebf9963356c4a8f0846c7
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-16 11:28:42 +0530

    start working on seller layout page 1

--- frontend/src/components/seller/SellerLogin.jsx
+++ frontend/src/components/seller/SellerLogin.jsx
@@ -40,29 +40,28 @@ const SellerLogin = () => {
 
     return !isSeller && (
         <form onSubmit={onSubmitHandler} className='min-h-screen flex items-center text-sm text-gray-600'>
+            <div className='flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200'>
 
-            <div className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
-
-                <p className="text-2xl font-medium m-auto">
-                    <span className="text-primary">User</span> Login
-                </p>
+                <p className='text-2xl font-medium m-auto'><span className="text-primary">Seller</span> Login</p>
 
                 <div className="w-full ">
                     <p>Email</p>
-                    <input onChange={(e) => setEmail(e.target.value)} value={email} placeholder="enter your email" className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" type="email" required />
+                    <input onChange={(e) => setEmail(e.target.value)} value={email}
+                        type="email" placeholder="enter you email"
+                        className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" required />
                 </div>
 
                 <div className="w-full ">
                     <p>Password</p>
-                    <input onChange={(e) => setPassword(e.target.value)} value={password} placeholder="enter your password" className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" type="password" required />
+                    <input onChange={(e) => setPassword(e.target.value)} value={password}
+                        type="password" placeholder="enter your password"
+                        className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary" required />
                 </div>
 
-                <button className="bg-primary hover:bg-primary-dull transition-all text-white w-full py-2 rounded-md cursor-pointer">
-                    Login
-                </button>
-
+                <button className="bg-primary text-white w-full py-2 rounded-md cursor-pointer">Login</button>
             </div>
-        </form>
+
+        </form >
     )
 }
 
--- frontend/src/pages/seller/SellerLayout.jsx
+++ frontend/src/pages/seller/SellerLayout.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useAppContext } from '../../context/AppContext';
+import { assets } from '../../assets/assets';
+import { Link, NavLink } from 'react-router-dom';
 
 const SellerLayout = () => {
     const { currency,
@@ -18,50 +20,49 @@ const SellerLayout = () => {
         searchQuery,
         setSearchQuery } = useAppContext()
 
-
-    const overviewicon = (
-        <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M7.111 20A3.111 3.111 0 0 1 4 16.889v-12C4 4.398 4.398 4 4.889 4h4.444a.89.89 0 0 1 .89.889v12A3.111 3.111 0 0 1 7.11 20Zm0 0h12a.889.889 0 0 0 .889-.889v-4.444a.889.889 0 0 0-.889-.89h-4.389a.889.889 0 0 0-.62.253l-3.767 3.665a.933.933 0 0 0-.146.185c-.868 1.433-1.581 1.858-3.078 2.12Zm0-3.556h.009m7.933-10.927 3.143 3.143a.889.889 0 0 1 0 1.257l-7.974 7.974v-8.8l3.574-3.574a.889.889 0 0 1 1.257 0Z" />
-        </svg>
-    );
-
-    const chaticon = (
-        <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 9h5m3 0h2M7 12h2m3 0h5M5 5h14a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1h-6.616a1 1 0 0 0-.67.257l-2.88 2.592A.5.5 0 0 1 8 18.477V17a1 1 0 0 0-1-1H5a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z" />
-        </svg>
-    );
-
     const sidebarLinks = [
-        { name: "Dashboard", path: "/", icon: dashboardicon },
-        { name: "Overview", path: "/overview", icon: overviewicon },
-        { name: "Chat", path: "/chat", icon: chaticon },
+        { name: "Add Product", path: "/seller", icon: assets.add_icon },
+        { name: "Product List", path: "/seller/product-list", icon: assets.product_list_icon },
+        { name: "Orders", path: "/seller/orders", icon: assets.order_icon },
     ];
+
+    const logout = async () => {
+        setIsSeller(false)
+    }
+
 
     return (
         <>
-            <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white transition-all duration-300">
-                <a href="https://prebuiltui.com">
-                    <img className="h-9" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoColored.svg" alt="dummyLogoColored" />
-                </a>
+            <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white">
+
+                <Link to="/">
+                    <img src={assets.logo} alt="logo" className='cursor-pointer w-34 md:w-38' />
+                </Link>
+
                 <div className="flex items-center gap-5 text-gray-500">
                     <p>Hi! Admin</p>
-                    <button className='border rounded-full text-sm px-4 py-1'>Logout</button>
+                    <button onClick={logout} className='border rounded-full text-sm px-4 py-1'>Logout</button>
                 </div>
             </div>
-            <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
-                {sidebarLinks.map((item, index) => (
-                    <a href={item.path} key={index}
-                        className={`flex items-center py-3 px-4 gap-3 
-                            ${index === 0 ? "border-r-4 md:border-r-[6px] bg-indigo-500/10 border-indigo-500 text-indigo-500"
-                                : "hover:bg-gray-100/90 border-white text-gray-700"
-                            }`
-                        }
-                    >
-                        {item.icon}
-                        <p className="md:block hidden text-center">{item.name}</p>
-                    </a>
-                ))}
+
+            <div className='flex'>
+                <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
+                    {sidebarLinks.map((item) => (
+                        <NavLink to={item.path} key={item.name} end={item.path === "/seller"}
+                            className={({ isActive }) => `flex items-center py-3 px-4 gap-3 
+                            ${isActive ? "border-r-4 md:border-r-[6px] bg-primary/10 border-primary text-primary"
+                                    : "hover:bg-gray-100/90 border-white"
+                                }`
+                            }
+                        >
+                            <img src={item.icon} alt="" className="w-7 h-7" />
+                            <p className="md:block hidden text-center">{item.name}</p>
+                        </NavLink>
+                    ))}
+                </div>
+                {/* <Outlet/> */}
             </div>
+            
         </>
     );
 };
