a22c0dc8c8986f97d89b20cb4847870eabc80679
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-18 00:15:34 +0530

    fix star icon key in Product details page

--- frontend/src/pages/ProductDetails.jsx
+++ frontend/src/pages/ProductDetails.jsx
@@ -74,7 +74,7 @@ const ProductDetails = () => {
 
                         <div className="flex items-center gap-0.5 mt-1">
                             {Array(5).fill('').map((_, i) => (
-                                <img src={i < 4 ? assets.star_icon : assets.star_dull_icon} alt=''
+                                <img key={i} src={i < 4 ? assets.star_icon : assets.star_dull_icon} alt=''
                                     className='md:w-4 w-3.5' />
                             ))}
 
