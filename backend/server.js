b0ce24c0f505f6eb6d647bd4b5cc3cb51abde320
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-18 17:27:50 +0530

    add middlewares like express.json, cookieParser, cors in server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
+import cookieParser from 'cookie-parser'
 import express from 'express'
 
 const port = process.env.PORT || 4000
 const app = express()
+
+// Allow multiple origins
+const allowedOrigins=["http://localhost:5173"]
+
+// Middleware configuration
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors({
+  origin:allowedOrigins,
+  Credentials:true
+}))
 
 app.get('/',(req,res)=>{
     res.send("Api is Working")
