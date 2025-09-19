8a2d4918674bce9b51d7c864399ee32ae2205399
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-19 22:46:09 +0530

    add product.model.js in models folder and within it define the product schema

---
+++ backend/models/product.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const productSechma = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    offerPrice: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: Array,
+        required: true
+    },
+    description: {
+        type: Array,
+        required: true,
+        unique:true
+    },
+    inStock: {
+        type: Boolean,
+        default: true
+    }
+},
+    {
+        // Automatically add createdAt and updatedAt fields
+        timestamps: true
+    })
+
+// Reuse the model if it already exists (prevents OverwriteModelError in hot reload)
+const Product = mongoose.models.Product || mongoose.model('Product', userSchema);
+
+export default Product;
