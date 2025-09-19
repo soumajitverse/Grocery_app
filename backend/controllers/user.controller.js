c8f78db202ea405a97acbb3e0105b55ba575c3ca
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-19 10:33:03 +0530

    in login handler check the user given password and stored hashed password in DB is same or not. If it's same then create a token

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -62,6 +62,7 @@ export const register = async (req, res) => {
 
     } catch (error) {
         res.status(500).json({
+            success: false,
             message: "internal server error",
             error
         })
@@ -71,27 +72,67 @@ export const register = async (req, res) => {
 
 // Login User : api/user/login
 export const login = async (res, req) => {
-  try {
-    const {email, password} = req.body
+    try {
+        const { email, password } = req.body
 
-    // to check all the details are given or not
-    if(!email || !password){
-        return res.status(400).json({
-            success:false,
-            message:"Email and password are required to login"
+        // to check all the details are given or not
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email and password are required to login"
+            })
+        }
+
+        // checking user is already present or not
+        let existingUser = await User.findOne({ email })
+        if (!existingUser) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid email or password"
+            })
+        }
+
+
+        // checking the user given password while login and the hashed password is same or not
+        let isMatch = await bcrypt.compare(password, existingUser.password)
+
+        if (!isMatch) {
+            return res.status(400).json({
+                success: false,
+                message: "Incorrect Password"
+            })
+        }
+
+        // if both the password is same then it will create a token
+        // jwt Token Created And Stored The Value into Token
+        let token;
+        try {
+            token = generateToken(existingUser._id)
+        } catch (error) {
+            console.log(error)
+        }
+
+        // Cookie Creation
+        res.cookie('token', token, {
+            httpOnly: true, // Prevent JavaScript to access cookie
+            secure: process.env.NODE_ENV === 'production', // Use secure cookie in the production
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // CSRF protection
+            maxAge: 7 * 24 * 60 * 60 * 1000 // this is cookie expiration time and this is always be written in milisecond. 
+        })
+
+        return res.status(200).json({
+            success: true,
+            user: {
+                email: existingUser.email,
+                name: existingUser.name
+            }
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "internal server error",
+            error
         })
     }
-
-    // checking user is already present or not
-    let existingUser = await User.findOne({email})
-    if(!existingUser){
-        return res.status(400).json({
-            success:false,
-            message:"Invalid email or password"
-        })
-    }
-
-  } catch (error) {
-    
-  }
 }
