a30c690878a1a17c61c868e1b8e9758afaa5ac5d
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-20 22:12:18 +0530

    extract the productData value from req.body after parse it into json

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,21 +1,26 @@
 
 // Add Product : /api/product/add
-export const addProduct = async(req, res)=>{
-    
+export const addProduct = async (req, res) => {
+    try {
+        let productData = JSON.parse(req.body.productData) 
+
+    } catch (error) {
+
+    }
 }
 
 // Get Product : /api/product/list
-export const productList = async(req, res)=>{
+export const productList = async (req, res) => {
 
 }
 
 // Get Single Product : /api/product/id
-export const productById = async(req, res)=>{
+export const productById = async (req, res) => {
 
 }
 
 // Change Product inStock : /api/product/stock
-export const changeStock = async(req, res)=>{
+export const changeStock = async (req, res) => {
 
 }
 
