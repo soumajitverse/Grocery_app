9117542def39d0f70ea4f181746cd5efb89af476
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-20 21:53:57 +0530

    add a upload instance of multer

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -1,2 +1,5 @@
 import multer from " multer";
 
+// upload → an instance of multer you can use as middleware in your routes.
+export const upload = multer({ storage: multer.diskStorage({}) }) // tells multer to store uploaded files directly on your disk (in a folder like /uploads by default, unless you customize it).
+
