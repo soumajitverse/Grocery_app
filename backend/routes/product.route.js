45b59b268a1fa1e20131bbd9ed29b694007eacd2
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-21 11:55:12 +0530

    add /id route in product.routes.js

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { upload } from "../config/multer.js";
 import sellerAuth from "../middlewares/seller.auth.js";
-import { addProduct, productList } from "../controllers/product.controller";
+import { addProduct, productById, productList } from "../controllers/product.controller";
 
 const productRouter = express.Router()
 
 productRouter.post('/add', upload.array([images]), sellerAuth, addProduct)
 productRouter.get('/list', productList)
+productRouter.get('/id', productById)
