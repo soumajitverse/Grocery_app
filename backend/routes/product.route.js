281bb4f63331f98b0b4e90e42854c75563bc1de9
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-21 13:50:12 +0530

    fix product.route.js and start work on cartController

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -4,8 +4,11 @@ import sellerAuth from "../middlewares/seller.auth.js";
 import { addProduct, changeStock, productById, productList } from "../controllers/product.controller.js";
 
 const productRouter = express.Router()
+ 
+// Reason
+// productRouter.post('/add', upload.array([images]), sellerAuth, addProduct)
+productRouter.post('/add', upload.array(['images']), sellerAuth, addProduct)
 
-productRouter.post('/add', upload.array([images]), sellerAuth, addProduct)
 productRouter.get('/list', productList)
 productRouter.get('/id', productById)
 productRouter.post('/stock', sellerAuth, changeStock)
