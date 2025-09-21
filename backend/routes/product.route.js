7c4b5feb853df7276b4cb8718d17c53f06f89708
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-21 11:56:49 +0530

    add /stock route in product.routes.js

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { upload } from "../config/multer.js";
 import sellerAuth from "../middlewares/seller.auth.js";
-import { addProduct, productById, productList } from "../controllers/product.controller";
+import { addProduct, changeStock, productById, productList } from "../controllers/product.controller";
 
 const productRouter = express.Router()
 
 productRouter.post('/add', upload.array([images]), sellerAuth, addProduct)
 productRouter.get('/list', productList)
 productRouter.get('/id', productById)
+productRouter.post('/stock', sellerAuth, changeStock)
