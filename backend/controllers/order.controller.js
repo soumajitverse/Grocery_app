b414dda23b298b02542b40264b3e3bdb7c66cd6d
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-21 19:32:35 +0530

    add getuserOrders handler for api end point /api/order/user

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -43,3 +43,23 @@ export const placeOrderCOD = async (req, res) => {
         })
     }
 }
+
+
+// Get Orders by User ID : /api/order/user
+export const getUserOrders = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const orders = await Order.find({
+            userId,
+            $or: [{ paymentType: "COD" }, { isPaid: true }]
+        }).populate("items.product address").sort({ createdAt: -1 });
+        res.status(200).json({
+             success: true, orders 
+            });
+    } catch (error) {
+       return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
