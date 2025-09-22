e5d9ccb92487291607e2947f175f99c598caa509
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-22 10:24:31 +0530

    fix the syntax of multer middleware code in product.route.js

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -5,9 +5,7 @@ import { addProduct, changeStock, productById, productList } from "../controller
 
 const productRouter = express.Router()
  
-// Reason
-// productRouter.post('/add', upload.array([images]), sellerAuth, addProduct)
-productRouter.post('/add', upload.array(['images']), sellerAuth, addProduct)
+productRouter.post('/add', upload.array('images'), sellerAuth, addProduct)
 
 productRouter.get('/list', productList)
 productRouter.get('/id', productById)
