a61e977ce2232ed00e7005f3ffaff4e6871a5c44
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-23 16:21:22 +0530

    add code to change filename of the image

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,14 +1,15 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination:(req, file, cb) => {
-        cb(null,"./public")
+    destination: (req, file, cb) => {
+        cb(null, "./public")
     },
-    filename:(req, file, cb) => {
+    filename: (req, file, cb) => {
         console.log(file)
-        cb(null, file.originalname)
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + '-' + file.originalname);
     }
 })
 
 
-export const upload = multer({storage})
+export const upload = multer({ storage })
