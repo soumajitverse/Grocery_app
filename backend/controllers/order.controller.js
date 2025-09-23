b7fa137279cccd1d831c0591eb53049ee4fbf43d
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-23 08:55:15 +0530

    add getAllOrders handler for api end point /api/order/seler

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -60,8 +60,29 @@ export const getUserOrders = async (req, res) => {
             userId,
             $or: [{ paymentType: "COD" }, { isPaid: true }]
         }).populate("items.product address").sort({ createdAt: -1 });
+
         res.status(200).json({
-            success: true, orders
+            success: true,
+            orders
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+// Get All Orders (for seller / admin) : /api/order/seller
+export const getAllOrders = async (req, res) => {
+    try {
+        const orders = await Order.find({
+            $or: [{ paymentType: "COD" }, { isPaid: true }]
+        }).populate("items.product address").sort({ createdAt: -1 });
+
+        res.status(200).json({
+            success: true,
+            orders
         });
     } catch (error) {
         return res.status(500).json({
