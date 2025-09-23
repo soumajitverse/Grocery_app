053ad883a57275a542c6b7a62aabf1b0d4e96ed2
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-23 09:48:53 +0530

    import axios and pass it through the context api

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyProducts } from "../assets/assets";
 import toast from "react-hot-toast";
-
+import axios from "axios"
 
 export const AppContext = createContext()
 
@@ -98,7 +98,8 @@ export const AppContextProvider = ({ children }) => {
         searchQuery,
         setSearchQuery,
         getCartCount,
-        getCartAmount
+        getCartAmount,
+        axios
     }
 
     return (
