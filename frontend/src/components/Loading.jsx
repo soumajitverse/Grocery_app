20d7817ebaba9a03918ad2ed37dde0632f3f25cf
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-09-27 22:25:37 +0530

    change the size of the loading animation

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -14,14 +14,14 @@ const Loading = () => {
         if (nextUrl) {
             setTimeout(() => {
                 navigate(`/${nextUrl}`) // it will redirct to my-orders
-            }, 3000)
+            }, 5000)
         }
     }, [nextUrl])
 
 
     return (
         <div className='flex justify-center items-center h-screen flex-col'>
-            <div className='animate-spin rounded-full h-12 w-12 sm:w-16 sm:h-16 border-4 border-gray-300 border-t-primary'></div>
+            <div className='animate-spin rounded-full h-12 w-12 sm:w-14 sm:h-14 border-4 border-gray-300 border-t-primary'></div>
             <div className='mt-2'>Please wait...</div>
         </div>
     )
