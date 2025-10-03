6cc8c1f9782e0506280d6559f1254e472b58908a
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-10-04 02:12:32 +0530

    change some styles

--- frontend/src/components/BottomBanner.jsx
+++ frontend/src/components/BottomBanner.jsx
@@ -4,18 +4,18 @@ import { assets, features } from '../assets/assets'
 const BottomBanner = () => {
     return (
         <div className='relative mt-24'>
-            <img src={assets.bottom_banner_image} alt="banner" className='w-full hidden md:block' />
-            <img src={assets.bottom_banner_image_sm} alt="banner" className='w-full md:hidden' />
+            <img src={assets.bottom_banner_image} alt="banner" className='w-full hidden md:block rounded-xl' />
+            <img src={assets.bottom_banner_image_sm} alt="banner" className='w-full md:hidden rounded-xl' />
 
             <div className='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24'>
                 <div>
-                    <h1 className='text-2xl md:text-3xl font-semibold text-primary mb-6'>Why We Are the Best?</h1>
+                    <h1 className='text-2xl md:text-3xl font-bold text-gray-700 mb-6'>Why We Are the Best?</h1>
                     {features.map((feature, index) => (
                         <div key={index} className='flex items-center gap-4 mt-2'>
                             <img src={feature.icon} alt={feature.title} className='md:w-11 w-9' />
                             <div>
                                 <h3 className='text-lg md:text-xl font-semibold'>{feature.title}</h3>
-                                <p className='text-gray-500/70 text-xs md:text-sm'>{feature.description}</p>
+                                <p className='text-gray-800/70 font-semibold text-sm md:text-sm'>{feature.description}</p>
                             </div>
 
                         </div>
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,11 +3,11 @@ import { assets, footerLinks } from "../assets/assets";
 const Footer = () => {
 
     return (
-        <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-24 bg-primary/10">
+        <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-24 bg-primary/25">
             <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
                 <div>
                     <img className="w-34 md:w-32" src={assets.logo} alt="logo" />
-                    <p className="max-w-[410px] mt-6">
+                    <p className="max-w-[410px] mt-6 text-gray-700/90">
                        From fresh produce to daily needs, we deliver it all straight to your home. Reliable, affordable, and trusted by thousands of happy customers.</p>
                 </div>
                 <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
@@ -17,7 +17,7 @@ const Footer = () => {
                             <ul className="text-sm space-y-1">
                                 {section.links.map((link, i) => (
                                     <li key={i}>
-                                        <a href={link.url} className="hover:underline transition">{link.text}</a>
+                                        <a href={link.url} className="hover:underline transition text-gray-700/90">{link.text}</a>
                                     </li>
                                 ))}
                             </ul>
@@ -25,7 +25,7 @@ const Footer = () => {
                     ))}
                 </div>
             </div>
-            <p className="py-4 text-center text-sm md:text-base text-gray-500/80">
+            <p className="py-4 text-center text-sm md:text-base text-gray-700/90">
                 Copyright {new Date().getFullYear()} © soumajitverse.dev All Right Reserved.
             </p>
         </div>
