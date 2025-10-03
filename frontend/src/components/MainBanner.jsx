f53f803e37603abb0ab15873ac03c750872889bd
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-10-04 01:27:28 +0530

    change the main banner image for desktop and mobile

--- frontend/src/components/MainBanner.jsx
+++ frontend/src/components/MainBanner.jsx
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom'
 const MainBanner = () => {
   return (
     <div className='relative z-0'>
-      <img src={assets.main_banner_bg} alt="banner" className='w-full hidden md:block'/>
-      <img src={assets.main_banner_bg_sm} alt="banner" className='w-full md:hidden'/>
+      <img src={assets.main_banner_bg} alt="banner" className='w-full hidden md:block rounded-xl'/>
+      <img src={assets.main_banner_bg_sm} alt="banner" className='w-full md:hidden rounded-xl'/>
 
       <div className='absolute inset-0 flex flex-col items-center md:items-start justify-end md:justify-center pb-24 md:pb-0 px-4 md:pl-18 lg:pl-24'>
         <h1 className='text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15'
@@ -19,7 +19,7 @@ const MainBanner = () => {
         <img className='md:hidden transition group-focus:translate-x-1' src={assets.white_arrow_icon} alt="arrow" />
         </Link>
 
-        <Link to={"/products"} className='group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer'>
+        <Link to={"/products"} className='group hidden md:flex items-center gap-2 px-9 py-3 cursor-pointer font-bold'>
         Explore deals
         <img className='transition group-hover:translate-x-1' src={assets.black_arrow_icon} alt="arrow" />
         </Link>
