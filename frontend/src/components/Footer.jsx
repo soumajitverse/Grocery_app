b35ea9534b3ec5632697ed4be8f004c1a1107415
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-10-05 10:16:47 +0530

    change name

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="py-4 text-center text-sm md:text-base text-gray-700/90">
-                Copyright {new Date().getFullYear()} © soumajitverse.dev All Right Reserved.
+                Copyright {new Date().getFullYear()} © soumajitverse All Right Reserved.
             </p>
         </div>
     );
