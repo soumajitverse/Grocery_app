8d16996dbcccdb7f84d0fe2c5b3e55a0db803c7e
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-10-11 23:30:26 +0530

    add insta, fb and x logo

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
                 <div>
                     <img className="w-34 md:w-32" src={assets.logo} alt="logo" />
                     <p className="max-w-[410px] mt-6 text-gray-700/90">
-                       From fresh produce to daily needs, we deliver it all straight to your home. Reliable, affordable, and trusted by thousands of happy customers.</p>
+                        From fresh produce to daily needs, we deliver it all straight to your home. Reliable, affordable, and trusted by thousands of happy customers.</p>
                 </div>
                 <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
                     {footerLinks.map((section, index) => (
@@ -17,7 +17,12 @@ const Footer = () => {
                             <ul className="text-sm space-y-1">
                                 {section.links.map((link, i) => (
                                     <li key={i}>
-                                        <a href={link.url} className="hover:underline transition text-gray-700/90">{link.text}</a>
+                                        <a href={link.url} className="hover:underline transition text-gray-700/90">
+                                            {link.text}
+                                            <img
+                                                className="ml-4 mb-2"
+                                                src={link.logo} alt="" />
+                                        </a>
                                     </li>
                                 ))}
                             </ul>
@@ -26,7 +31,7 @@ const Footer = () => {
                 </div>
             </div>
             <p className="py-4 text-center text-sm md:text-base text-gray-700/90">
-                Copyright {new Date().getFullYear()} © soumajitverse All Right Reserved.
+                Copyright {new Date().getFullYear()} © soumajitverse. All Right Reserved.
             </p>
         </div>
     );
