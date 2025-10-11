ead9439b0fcc30c1a482e9c277af355f2a604a62
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-10-11 23:29:11 +0530

    add textarea for type message

--- frontend/src/components/InputField.jsx
+++ frontend/src/components/InputField.jsx
@@ -1,7 +1,16 @@
 import React from "react"
 
 const InputField = ({ type, placeholder, name, handleChange }) => {
-  return (
+  return name === 'message' ? (
+    <textarea
+      className='w-full px-2 py-2.5 border border-gray-500/30 rounded-xl outline-none text-gray-500 focus:border-primary transition h-40'
+      type={type}
+      placeholder={placeholder}
+      name={name}
+      onChange={handleChange}   // this triggers handleChange when user types
+      required
+    />
+  ) : (
     <input
       className="w-full px-2 py-2.5 border border-gray-500/30 rounded-xl outline-none text-gray-500 focus:border-primary transition"
       type={type}
