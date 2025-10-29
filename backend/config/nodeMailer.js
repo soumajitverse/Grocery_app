00aaac9c92320dae3d308f219819c8921284ae10
Soumajit Majumder <majumdersoumajit09@gmail.com>
2025-10-29 23:09:29 +0530

    test on deploy

--- backend/config/nodeMailer.js
+++ backend/config/nodeMailer.js
@@ -2,6 +2,10 @@ import dotenv from 'dotenv'
 dotenv.config()
 import nodemailer from "nodemailer";
 
+console.log("for testing smtp_host: ", process.env.SMTP_HOST)
+console.log("for testing smtp_port: ", process.env.SMTP_PORT)
+console.log("for testing smtp_user: ", process.env.SMTP_USER)
+
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: Number(process.env.SMTP_PORT),
